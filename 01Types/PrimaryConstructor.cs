fb1d3a7d97f736ef84b757d22fbdcb08eb13853e
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2024-08-28 19:46:47 +0300

    delegates

--- 01Types/PrimaryConstructor.cs
+++ 01Types/PrimaryConstructor.cs
@@ -1,32 +1,37 @@
 record Movie(string Title, int Length);
 
-class School(string name)
-// by passing values here, we have created an automatic constructor that takes name
+namespace refreshingCSharp.Types
 {
-    public string Name { get; set; } = name;
-    public string Location { get; set; }
-    public int NoOfStudents { get; init; }
 
-    // now every-constructor must invoke the automatic constructor, making sure name is always set
-    public School(string name, string location)
-        : this(name)
+    class School(string name)
+    // by passing values here, we have created an automatic constructor that takes name
     {
-        Location = location;
+        public string Name { get; set; } = name;
+        public string Location { get; set; }
+        public int NoOfStudents { get; init; }
+
+        // now every-constructor must invoke the automatic constructor, making sure name is always set
+        public School(string name, string location)
+            : this(name)
+        {
+            Location = location;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} from {Location}";
+        }
     }
 
-    public override string ToString()
+    class PrimaryConstructor
     {
-        return $"{Name} from {Location}";
+        public static void Run()
+        {
+            var s = new School("moi FA", "Huruma");
+            WriteLine(s);
+            var s1 = new School("Moi Girls") { Location = "Nairobi", NoOfStudents = 32 };
+            var m = new Movie ("Pulp Fiction", 20);
+        }
     }
 }
 
-class PrimaryConstructor()
-{
-    public static void Run()
-    {
-        var s = new School("moi FA", "Huruma");
-        WriteLine(s);
-        var s1 = new School("Moi Girls") { Location = "Nairobi", NoOfStudents = 32 };
-        var m = new Movie { Title = "Pulp fiction", Length = 20 };
-    }
-}
---
+++ 04Complex/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+namespace _04Complex;
+
+// A delegate is an object that knows how to call a function
+// this transformer is compatible with any method that takes an int and returns an int
+delegate T Transformer<T>(T x);
+
+// Adding a ref keyword before a struct ensures that the struct will always live on the stack and not the heap
+ref struct Operation<T>
+{
+    public string? Name { get; set; }
+    public Transformer<T>? Op { get; set; }
+}
+
+
+class Program
+{
+   
+    static T Transform<T>(T value, Operation<T> o)
+    {
+        if (o.Op == null) {
+            throw new ArgumentException("cannot provide operation with null values");
+        }
+        T res = o.Op(value);
+        WriteLine($"Transformation [{o.Name}] {value} to {res}");
+        return res;
+    }
+
+    static void Main()
+    {
+        var operation = new Operation<double> { Name = "Tan", Op = Math.Tan };
+        double _ = Transform(10.223, operation);
+    }
+}
+
