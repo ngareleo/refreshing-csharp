48ac6283efd7dca842afad1deff8283ec0174f52
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2024-09-02 21:11:23 +0300

    linq

---
+++ 06LINQ/Program.cs
@@ -0,0 +1,106 @@
+﻿
+namespace _06LINQ;
+
+using static System.Console;
+using System.Linq;
+
+
+enum House { LANET, MOI, NANYUKI, MOMBASA }
+enum Grade { A, B, C, D, E }
+
+record Student(string Name, int Id, Grade? Grade, House House)
+{
+    public static House[] Houses = Enum.GetValues<House>();
+    public static int HousesCount = Houses.Length;
+
+    public static House GetRandomHouse() => Houses[new Random().Next(HousesCount)];
+
+    public override string ToString()
+    {
+        return $$""""
+               {
+                 name:  {{Name}},
+                 ID:    {{Id}},
+                 grade: {{Grade}},
+                 house: {{House}}
+               }
+            """";
+    }
+
+    /// <summary>A factory for students assigning them a random house.</summary>
+    public static IEnumerable<Student> StudentFactory(IEnumerable<string> names)
+        => names.Select((n) => new Student(n, 0, null, GetRandomHouse()));
+
+}
+
+class Program
+{
+
+    static void Main()
+    {
+        // IEnumerables are the basic unit for C#
+        string[] names = { "Harry", "Tom", "Jerry", "Tate", "Messi", "Obama" };
+        // This can be referred to as a local sequence because its a local collection of something
+
+
+        var shortNames = names.Where((n) => n.Length <= 3);
+        Pretty("\nShort names : ", shortNames);
+        var namesWithRs = names.Where((n) => n.ToUpper().Contains("R"));
+        Pretty("\nNames with R", namesWithRs);
+
+        var students = Student.StudentFactory(names);
+        Pretty(students);
+
+
+        var twoStudentsInNanyukiHouse = students.Where((s) => s.House == House.NANYUKI).Take(2);
+        // Values are captured during execution
+        students.Append(new Student("Vasily", 23, null, House.NANYUKI));
+
+        Pretty("\n\nTwo students in Nanyuki house ", twoStudentsInNanyukiHouse);
+
+        // This is defined as a query expression
+        var aStudentInLanet = (from s in students
+                               where s.House == House.LANET
+                               select s
+                               ).First();
+        WriteLine($"\n\nA student in Lanet {aStudentInLanet}");
+
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 21 };
+        var even = nums.Where((n) => n % 2 == 0);
+        // The enumerable will execute not when declared but when enumerated. Meaning, you read from the enumerator
+        // That's inside of Pretty
+
+        nums.Append(100);
+        // We inputs are captured during declaration. So you cannot inject anything after
+
+        Pretty(even);
+    }
+
+
+
+    static void Pretty<T>(string left, IEnumerable<T> e, string right = "")
+    {
+        WriteLine(left);
+        foreach (var v in e)
+        {
+            WriteLine(v);
+        }
+        if (!string.IsNullOrEmpty(right))
+        {
+            WriteLine("additional");
+        }
+    }
+    static void Pretty<T>(IEnumerable<T> e, string? right = null)
+    {
+        foreach (var v in e)
+        {
+            WriteLine(v);
+        }
+        if (!string.IsNullOrEmpty(right))
+        {
+            WriteLine("additional");
+        }
+
+    }
+}
+
---
+++ Collections/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using static System.Collections.IEnumerable;
+using static System.Console;
+
+namespace Collections;
+
+enum FinancePlan { government_sponsored, private_sponsered, scholarship }
+
+class Student : IComparable
+{
+    public string? Name { get; init; }
+    public FinancePlan? Plan { get; init; }
+    public int? ID { get; init; }
+
+    public int CompareTo(int? iD, Student? s)
+    {
+        return CompareTo(s.ID, ID);
+    }
+
+    public override string ToString()
+    {
+        return $"Student :> {Name} on {Plan}";
+    }
+}
+
+
+class Class : IEnumerable
+{
+    public DateTime YearStarted { get; init; }
+    public (int, int) CurrentYearAndSemester { get; init; }
+    public Student[]? Students { get; init; }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        foreach (var student in Students ?? Array.Empty<Student>())
+        {
+            yield return student;
+        }
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        var students = new Student[]{
+            new Student {Name="Denni", Plan=FinancePlan.government_sponsored},
+            new Student {Name="Allan", Plan=FinancePlan.scholarship},
+            new Student {Name="Wilkins", Plan=FinancePlan.private_sponsered}
+        };
+        Array.Reverse(students);
+        var firstClass = new Class { YearStarted = DateTime.Now, CurrentYearAndSemester = (1, 1), Students = students };
+
+        foreach (var s in firstClass)
+        {
+            WriteLine(s);
+        }
+    }
+}
+
