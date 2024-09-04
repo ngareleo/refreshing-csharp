466f0ce4d03ebfd13ac2b116f3901c39a5fb0227
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2024-09-05 00:12:22 +0300

    algos

--- 06LINQ/Program.cs
+++ 06LINQ/Program.cs
@@ -1,12 +1,24 @@
-﻿
-namespace _06LINQ;
+﻿namespace _06LINQ;
 
-using static System.Console;
 using System.Linq;
+using static System.Console;
 
+enum House
+{
+    LANET,
+    MOI,
+    NANYUKI,
+    MOMBASA,
+}
 
-enum House { LANET, MOI, NANYUKI, MOMBASA }
-enum Grade { A, B, C, D, E }
+enum Grade
+{
+    A,
+    B,
+    C,
+    D,
+    E,
+}
 
 record Student(string Name, int Id, Grade? Grade, House House)
 {
@@ -18,30 +30,76 @@ record Student(string Name, int Id, Grade? Grade, House House)
     public override string ToString()
     {
         return $$""""
-               {
                  name:  {{Name}},
                  ID:    {{Id}},
                  grade: {{Grade}},
-                 house: {{House}}
-               }
+                 house: {{House}},
             """";
     }
 
-    /// <summary>A factory for students assigning them a random house.</summary>
-    public static IEnumerable<Student> StudentFactory(IEnumerable<string> names)
-        => names.Select((n) => new Student(n, 0, null, GetRandomHouse()));
+    public static void PrettyPrintStudents(IEnumerable<Student> students)
+    {
+        int studentsPCol = 5;
+        int rowPStudent = 4;
+        var s = students.ToArray();
+        // We print students details, 5 students wide per screen width, downwards
+        // For each student we get a 2d array of bytes
 
+        Func<Student, char[][]> flatten = delegate(Student s)
+        {
+            var rows = s.ToString().Split('\n');
+            var res = new char[rowPStudent][];
+            for (int i = 0; i < rowPStudent; i++)
+            {
+                res[i] = rows[i].ToArray();
+            }
+            return res;
+        };
+
+        /// <summary>We assume that all nested arrays have same shape</summary>
+        Func<char[][][], int, char[][]> orderFlatList = delegate(char[][][] o, int max)
+        {
+            if (o.Length == 0)
+            {
+                return new char[0][];
+            }
+            var s = o[0].Length;
+            var totalRows = o.Length / (max + 1) * s + o.Length / (max + 1);
+            var result = new char[totalRows][];
+
+            for (int i = 0, iter = 0, depth = 0; i < totalRows; i++, iter = i / max, depth = i % s)
+            {
+                if (i % rowPStudent == 0)
+                {
+                    result[iter] = new char[] { '\n' };
+                    continue;
+                }
+                result[iter] = o[iter][depth];
+            }
+            return result;
+        };
+
+        var flatList = orderFlatList(students.Select(s => flatten(s)).ToArray(), studentsPCol);
+
+        foreach (var line in flatList)
+        {
+            var str = string.Join("", line);
+            WriteLine($"{str}");
+        }
+    }
+
+    /// <summary>A factory for students assigning them a random house.</summary>
+    public static IEnumerable<Student> StudentFactory(IEnumerable<string> names) =>
+        names.Select((n) => new Student(n, 0, null, GetRandomHouse()));
 }
 
 class Program
 {
-
     static void Main()
     {
         // IEnumerables are the basic unit for C#
-        string[] names = { "Harry", "Tom", "Jerry", "Tate", "Messi", "Obama" };
+        string[] names = { "Harry", "Tom", "Jerry", "Tate", "Charlene", "Obama" };
         // This can be referred to as a local sequence because its a local collection of something
-
 
         var shortNames = names.Where((n) => n.Length <= 3);
         Pretty("\nShort names : ", shortNames);
@@ -51,56 +109,72 @@ class Program
         var students = Student.StudentFactory(names);
         Pretty(students);
 
-
         var twoStudentsInNanyukiHouse = students.Where((s) => s.House == House.NANYUKI).Take(2);
-        // Values are captured during execution
-        students.Append(new Student("Vasily", 23, null, House.NANYUKI));
+        students.Append(new Student("Francis", 23, null, House.NANYUKI));
 
-        Pretty("\n\nTwo students in Nanyuki house ", twoStudentsInNanyukiHouse);
+        Student.PrettyPrintStudents(twoStudentsInNanyukiHouse);
 
         // This is defined as a query expression
-        var aStudentInLanet = (from s in students
-                               where s.House == House.LANET
-                               select s
-                               ).First();
+        var aStudentInLanet = (from s in students where s.House == House.LANET select s).First();
         WriteLine($"\n\nA student in Lanet {aStudentInLanet}");
 
-        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 21 };
-        var even = nums.Where((n) => n % 2 == 0);
-        // The enumerable will execute not when declared but when enumerated. Meaning, you read from the enumerator
-        // That's inside of Pretty
+        int factor = 2;
+        var nums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 21 }; // Query is declared here
+        var even = nums.Where((n) => n % factor == 0);
 
-        nums.Append(100);
-        // We inputs are captured during declaration. So you cannot inject anything after
+        factor = 3;
+        Pretty(even); // This is when the query is executed
 
-        Pretty(even);
+        // The enumerable will execute not when declared but when enumerated
+        // i.e. inside of Pretty
+
+        // There are exceptions to this rule,
+        // 1. Using a terminal Operators that return a single value such as Count() and First()
+        // 2. Using converter operators that convert the end result to an Object like `ToList`
+
+        var doubled = nums.Select((n) => n.ToString()).Aggregate((a, b) => $"{a}|{b}");
+
+        // The query is called lazily at this point
+        WriteLine($"Doubled: {doubled}");
+
+        // Imagine this query did something expensive like write some value to a file
+
+        // Iterating over the result would re-execute the query over and over. Costing you a tonne
+        var payload = nums.Select((n) => ExpensiveWriteToFile(n).ToString())
+            .Aggregate((acc, el) => $"{acc}, {el}");
+
+        WriteLine($"PayloadValue {payload}");
     }
 
+    static int ExpensiveWriteToFile(int value)
+    {
+        // Imagine this was an expensive write to a server
+        return value *= 4;
+    }
 
+    static void Pretty<T>(string left, IEnumerable<T> e)
+    {
+        var str = e.Where(s => s != null)
+            .Select(s => s?.ToString())
+            .Aggregate((acc, val) => $"{acc}, {val}");
+        WriteLine($"[ {left}\n{str} ]");
+    }
 
-    static void Pretty<T>(string left, IEnumerable<T> e, string right = "")
+    static void Pretty<T>(IEnumerable<T> e)
+    {
+        var str = e.Where(s => s != null)
+            .Select(s => s?.ToString())
+            .Aggregate((acc, val) => $"{acc}, {val}");
+        WriteLine($"[ \n{str} ]");
+    }
+
+    static void Pretty<T>(string left, IEnumerable<T> e, string right)
     {
         WriteLine(left);
         foreach (var v in e)
         {
             WriteLine(v);
         }
-        if (!string.IsNullOrEmpty(right))
-        {
-            WriteLine("additional");
-        }
-    }
-    static void Pretty<T>(IEnumerable<T> e, string? right = null)
-    {
-        foreach (var v in e)
-        {
-            WriteLine(v);
-        }
-        if (!string.IsNullOrEmpty(right))
-        {
-            WriteLine("additional");
-        }
-
+        WriteLine(right);
     }
 }
-
